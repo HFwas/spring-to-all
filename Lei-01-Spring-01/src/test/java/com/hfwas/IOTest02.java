3f179b205cfc3e7ce5c480768f299ddc782edb16
HFwas <1558934404@qq.com>
2021-10-01 23:03:20 +0800

    "fix://加载xml文件报错"

--- Lei-01-Spring-01/src/test/java/com/hfwas/IOTest02.java
+++ Lei-01-Spring-01/src/test/java/com/hfwas/IOTest02.java
@@ -1,5 +1,6 @@
 package com.hfwas;
 
+import com.hfwas.bean.Car;
 import com.hfwas.bean.Person;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
@@ -15,10 +16,21 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class IOTest02 {
     private static final ApplicationContext APPLICATION_CONTEXT1 = new ClassPathXmlApplicationContext("io2.xml");
 
+    // 测试使用null值，默认引用类型是就是null，基本类型是默认值
     @Test
     public void test01(){
-        Person person01 = APPLICATION_CONTEXT1.getBean("person01", Person.class);
+        Person person01 = APPLICATION_CONTEXT1.getBean("person06", Person.class);
+
+        //使用property赋值，比较结果false。使用复杂赋值，比较为true
+        System.out.println(person01.getLastnName() == null);
         System.out.println(person01);
+
+        //测试引用的对象和容器当中的对象是否是相同的对象
+        Car car01 = (Car) APPLICATION_CONTEXT1.getBean("car01");
+        Car car = person01.getCar();
+        // true:说明是一致的
+        System.out.println(car01 == car);
+
 
     }
 
